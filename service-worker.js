ed36733af89515c743e17b16c1d5f6f406ceb3ed
x2047 <138368953+x2047@users.noreply.github.com>
2023-12-20 14:55:47 +0900

    sample

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // キャッシュしたいファイルの一覧を指定 — (*1)
-const cacheFiles = ['index.html', 'logo.svg'];
+const cacheFiles = ['index.html', 'icon.jpg'];
 const cacheName = 'v1';
 // インストール時に実行されるイベント — (*2)
 self.addEventListener('install', event => {
@@ -12,17 +12,17 @@ self.addEventListener('activate', event => {
 });
 // fetchイベント
 self.addEventListener('fetch', event => {
-// キャッシュがあればそれを返す — (*3)
-event.respondWith(
-caches.match(event.request).then(function(resp) {
-return resp || fetch(event.request).then(function(response) {
-let responseClone = response.clone();
-caches.open(cacheName).then(function(cache) {
-cache.put(event.request, responseClone);
-});
-return response;
-});
-}).catch(function() {
-return caches.match('logo.svg');
-}));
+    // キャッシュがあればそれを返す — (*3)
+    event.respondWith(
+        caches.match(event.request).then(function(resp) {
+            return resp || fetch(event.request).then(function(response) {
+                let responseClone = response.clone();
+                caches.open(cacheName).then(function(cache) {
+                    cache.put(event.request, responseClone);
+            });
+            return response;
+        });
+    }).catch(function() {
+        return caches.match('logo.svg');
+    }));
 });
